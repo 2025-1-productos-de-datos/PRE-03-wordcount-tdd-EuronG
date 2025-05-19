f62e0d92f8ec80c8bf78c00d5cf29e8b8e4402bd
EuronG <juandigiraldoj@gmail.com>
2025-05-19 00:25:41 -0500

    ---test git vscpde

--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -1,1 +1,21 @@
+import os
+import sys
 
+from homework.src.parse_args import parse_args
+
+
+def read_all_lines(input_folder):
+    
+    lines = []
+    for filename in os.listdir(input_folder):
+        file_path = os.path.join(input_folder, filename)
+        with open(file_path, "r", encoding="utf-8") as f:
+            lines.extend(f.readlines())
+    
+    return lines
+
+def main():
+    input_folder, output_folder = parse_args() 
+
+    print(f"Input folder: {input_folder}")
+    print(f"Output folder: {output_folder}")
